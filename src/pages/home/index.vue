9ca1982110261c89b5245d3ea91af4ee537fb124
戴强 <daiq@fxiaoke.com>
2018-08-01 15:50:54 +0800

    initialize

---
+++ src/pages/home/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="containner">
+        <ul>
+            <li v-for="(item, index) in list" :key="index">
+                <a :href="item.url">{{item.title}}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            list: [
+                {
+                    title: 'index',
+                    url: '/pages/index/main',
+                },
+                {
+                    title: 'todo',
+                    url: '/pages/todo/main',
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<style lang="less">
+
+</style>
